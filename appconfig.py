24d451268e6166a35d3d57db5488aabac0296914
Gie Spaepen <giespaepen@outlook.com>
2019-09-17 23:29:03 +0200

    BBR-51 Added azure client and configs

--- appconfig.py
+++ appconfig.py
@@ -9,3 +9,8 @@ class AppConfig:
         "ANNOTATION_CONFIRMED", "org.debarrage.dns.confirmed")
     namespace = os.getenv("NAMESPACE", "default")
 
+    az_tenant = os.getenv("AZ_TENANT", "")
+    az_subscription = os.getenv("AZ_SUBSCRIPTION", "")
+    az_user = os.getenv("AZ_USER", "")
+    az_pwd = os.getenv("AZ_PWD", "")
+
--- services/azurefunctions.py
+++ services/azurefunctions.py
@@ -1,12 +1,27 @@
 from appconfig import AppConfig
 from services.loggerfunctions import create_logger
-
+from azure.mgmt.dns import DnsManagementClient
+from azure.common.credentials import UserPassCredentials
+from azure.common.credentials import ServicePrincipalCredentials
 
 # Instantiate logging
 log = create_logger("azure")
 
+def az_credentials(appConfig: AppConfig):
+    return ServicePrincipalCredentials(
+        client_id=appConfig.az_user,
+        secret=appConfig.az_pwd,
+        tenant=appConfig.az_tenant
+    )
+
+def az_client(appConfig: AppConfig):
+    return DnsManagementClient(
+        az_credentials(appConfig),
+        appConfig.az_subscription
+    )
 
 def create_dns(subdomain, ip, appConfig: AppConfig):
+    client = az_client(appConfig)
     log.info("Creating")
 
 
